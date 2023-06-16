7b4b9a320d1c9d123bb101e02562bacf3278cc2a
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-06-16 14:25:27 +0900

    Design: ProgressBar 컴포넌트 UI 구현 및 기능 구현

---
+++ src/components/target/ProgressBar.tsx
@@ -0,0 +1,19 @@
+type Props = {
+	completed: number;
+};
+
+const ProgressBar = ({ completed }: Props) => {
+	const progressWidth = `w-[${completed}%]`;
+
+	return (
+		<div className="h-8 w-full bg-[#e0e0de] rounded-md">
+			<div
+				className={`h-full ${progressWidth} bg-main rounded-md flex justify-center items-center`}
+			>
+				<span className="p-1 text-white font-semibold">{`${completed}%`}</span>
+			</div>
+		</div>
+	);
+};
+
+export default ProgressBar;
--- src/components/target/TargetForm.tsx
+++ src/components/target/TargetForm.tsx
@@ -1,19 +1,24 @@
 import logo from "../../assets/logo/거북.jpeg";
+import ProgressBar from "./ProgressBar";
 
 const TargetForm = () => {
 	const goal = "나는 이번 달에 5kg할꺼야~";
+
+	// 일의자리수가 0이도록 유틸 함수를 추후에 만들어야할 것 같다.
+	const successPercentage = 40;
+	const votePercentage = 30;
 	return (
 		<div className="mt-10 p-6 w-full min-h-[200px] border-2 border-mainHover rounded-md">
 			<h2 className="font-medium">{goal}</h2>
 			<img src={logo} alt="자그마한 로고 사진" className="w-48 mx-auto" />
 			<div className="flex">
-				<div className="flex flex-col w-1/2">
+				<div className="flex flex-col w-1/2 p-5">
 					<label>진행률</label>
-					<span>퍼센트</span>
+					<ProgressBar completed={successPercentage} />
 				</div>
-				<div className="flex flex-col w-1/2">
+				<div className="flex flex-col w-1/2 p-5">
 					<label>투표율</label>
-					<span>퍼센트</span>
+					<ProgressBar completed={votePercentage} />
 				</div>
 			</div>
 		</div>
