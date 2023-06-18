0c8fc192da4a0272cbabba52b14f3a377043a223
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-06-18 10:37:31 +0900

    Chore : react-datePicker 라이브러리 패키지 설치

--- src/components/goal/Duration.tsx
+++ src/components/goal/Duration.tsx
@@ -15,12 +15,21 @@ const Duration = ({ setStep }: Props) => {
 	} = useFormContext();
 	return (
 		<TargetCreateLayout title="언제까지 목표를 달성하실껀가요?">
+			<input
+				type="text"
+				className="placeholder:text-s w-full h-10 outline-none text-emerald-800 border-b-2 border-main"
+				placeholder="목표를 작성해주세요"
+				{...register("duration")}
+			/>
+
 			<button
+				className={`w-full h-16 text-xl bg-main px-10 py-2 mt-10 text-white rounded-xl`}
 				onClick={() => {
 					setStep("lastStep");
 				}}
+				type="button"
 			>
-				버튼
+				다음으로 가기
 			</button>
 		</TargetCreateLayout>
 	);
