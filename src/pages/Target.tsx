aaa628776cd239552d49e013b9dca888f25724ea
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-06-20 15:22:07 +0900

    Design : 전역 컨텍스트를 활용하여 Modal 외부 CSS 설정

--- src/pages/Target.tsx
+++ src/pages/Target.tsx
@@ -6,7 +6,8 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import { useInfo } from "../hooks/useInfo";
 import { useQuery } from "@tanstack/react-query";
-import { ModalProvider } from "../context/ModalContext";
+import { ModalContext } from "../context/ModalContext";
+import { useContext } from "react";
 
 const Target = () => {
 	const navigate = useNavigate();
@@ -14,52 +15,54 @@ const Target = () => {
 	const { data: targets } = useQuery(["target", "all"], () => {
 		return infoService?.getAllTarget();
 	});
-	console.log("target", targets);
+	const { isOpen } = useContext(ModalContext);
+	console.log("isOpen", isOpen);
 
 	return (
-		<ModalProvider>
-			<div className="relative flex flex-col h-screen px-6 pb-10">
-				<Header />
-				<section className="flex flex-col mt-16">
-					<h1 className="font-semibold text-2xl">현재 타켓 목록</h1>
-					<div className="flex flex-row">
-						<Carousel className="w-full" useKeyboardArrows showThumbs={false}>
-							{targets?.map(
-								({
-									user_id,
-									goal,
-									subgoal_total,
-									success_count,
-									vote_total,
-									success_vote,
-								}) => {
-									return (
-										<TargetForm
-											key={user_id}
-											{...{
-												goal,
-												subgoal_total,
-												success_count,
-												vote_total,
-												success_vote,
-											}}
-										/>
-									);
-								}
-							)}
-						</Carousel>
-					</div>
-					<button
-						className="mt-24 mx-auto w-1/3 h-14 bg-main rounded-lg"
-						onClick={() => {
-							navigate("/target/create");
-						}}
-					>
-						<FiEdit className="mx-auto text-white text-2xl" />
-					</button>
-				</section>
-			</div>
-		</ModalProvider>
+		<div className={`relative flex flex-col h-screen px-6 pb-10`}>
+			{isOpen && (
+				<div className="absolute inset-0 bg-opacity-50 bg-black z-10">1</div>
+			)}
+			<Header />
+			<section className="flex flex-col mt-16">
+				<h1 className="font-semibold text-2xl">현재 타켓 목록</h1>
+				<div className="flex flex-row">
+					<Carousel className="w-full" useKeyboardArrows showThumbs={false}>
+						{targets?.map(
+							({
+								user_id,
+								goal,
+								subgoal_total,
+								success_count,
+								vote_total,
+								success_vote,
+							}) => {
+								return (
+									<TargetForm
+										key={user_id}
+										{...{
+											goal,
+											subgoal_total,
+											success_count,
+											vote_total,
+											success_vote,
+										}}
+									/>
+								);
+							}
+						)}
+					</Carousel>
+				</div>
+				<button
+					className="mt-24 mx-auto w-1/3 h-14 bg-main rounded-lg"
+					onClick={() => {
+						navigate("/target/create");
+					}}
+				>
+					<FiEdit className="mx-auto text-white text-2xl" />
+				</button>
+			</section>
+		</div>
 	);
 };
 
