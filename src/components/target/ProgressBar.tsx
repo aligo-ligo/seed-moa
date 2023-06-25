0be5caee2cf885e86740da01ee8c98b79c1c5363
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-06-26 00:20:49 +0900

    Fix : 컴파일 css tailwind 내 inline style 추가하여 progress bar 에러 수정

--- src/components/target/ProgressBar.tsx
+++ src/components/target/ProgressBar.tsx
@@ -3,12 +3,13 @@ type Props = {
 };
 
 const ProgressBar = ({ completed = 0 }: Props) => {
-	const progressWidth = `w-[${completed}%]`;
+	console.log(completed);
 
 	return (
 		<div className="h-8 w-full bg-[#e0e0de] rounded-md">
 			<div
-				className={`h-full ${progressWidth} bg-main rounded-md flex justify-center items-center`}
+				className={`h-full bg-main rounded-md flex justify-center items-center`}
+				style={{ width: `${completed}%` }}
 			>
 				<span className="p-1 text-white font-semibold">{`${completed}%`}</span>
 			</div>
