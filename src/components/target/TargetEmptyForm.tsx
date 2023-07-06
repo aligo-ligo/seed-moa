d8f84b059a56a2f73bc9c3f8a449ce119cbaf48b
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-07-06 12:07:20 +0900

    Design : target이 없을때 보여질 페이지 UI 수정ㅇ

--- src/components/target/TargetEmptyForm.tsx
+++ src/components/target/TargetEmptyForm.tsx
@@ -3,12 +3,12 @@ import ProgressBar from "./ProgressBar";
 const TargetEmptyForm = () => {
 	return (
 		<>
-			<div className="mt-10 p-6 w-[350px] min-h-[200px] border-2 border-lightGray rounded-md cursor-pointer">
+			<div className="mt-10 p-6 w-[360px] min-h-[200px] border-2 border-lightGray rounded-md cursor-pointer">
 				<h2 className="font-medium text-center text-gray">
 					목표를 생성해주세요
 				</h2>
 				<img src={OliBodyImage} alt="자그마한 로고 사진" className=" mx-auto" />
-				<div className="flex w-60">
+				<div className="flex justify-center w-full">
 					<div className="flex flex-col w-1/2 p-2">
 						<label className="font-medium text-center text-gray">성취률</label>
 						<ProgressBar completed={0} />
