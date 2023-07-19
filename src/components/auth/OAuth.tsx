61184d9c77ebe7ef4f3517d20bf12b4b4df56b80
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-07-19 17:22:07 +0900

    Fix : ENV undefined 오류

--- src/components/auth/OAuth.tsx
+++ src/components/auth/OAuth.tsx
@@ -4,7 +4,7 @@ import kakaoLogo from "../../assets/kakao/kakao_login_medium_wide.png";
 const OAuth = () => {
 	const APP_KEY = import.meta.env.VITE_KAKAO_CLIENT_ID;
 	const REDIRECT_URL = import.meta.env.VITE_REDIRECT_URL;
-	console.log("re", REDIRECT_URL);
+
 	const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${APP_KEY}&redirect_uri=${REDIRECT_URL}&response_type=code`;
 	console.log(APP_KEY, REDIRECT_URL);
 	return (
