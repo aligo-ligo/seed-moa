94f45df52025b4d99ad1888dbafe301da24e9452
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-07-24 15:49:38 +0900

    Fix : 버튼 UI 수정

--- src/components/target/Checkbox.tsx
+++ src/components/target/Checkbox.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { CheckModalContext } from "../../context/CheckModalContext";
 import usePopUp from "../../hooks/usePopUp";
+import { FiCheck } from "react-icons/fi";
 
 type Props = {
 	children: React.ReactNode;
@@ -18,13 +19,19 @@ const Checkbox = ({ type, children, value, completedDate }: Props) => {
 	const { openModal, changeModalType } = usePopUp();
 
 	return (
-		<div className="flex my-5">
+		<div className={`flex my-5 ${completedDate && `text-gray line-through`} `}>
 			{type === "guest" && (
 				<button
-					className={`w-6 mr-3 border-2 p-2 text-orange-400 rounded-md ${
+					className={`w-6 mr-3 border-2 text-orange-400 rounded-md ${
 						completedDate && `bg-orange-400`
 					}`}
-				/>
+				>
+					{completedDate && (
+						<svg viewBox="0 0 26 26" className="fill-white">
+							<path d="M10 15.586l-3.293-3.293-1.414 1.414L10 18.414l9.707-9.707-1.414-1.414z" />
+						</svg>
+					)}
+				</button>
 			)}
 
 			{type === "detail" && (
