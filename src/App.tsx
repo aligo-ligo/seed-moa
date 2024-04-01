5cb8c37f3c5a3951209cbb3e95a9c3d41a833891
CodyMan0 <hys83751952@gmail.com>
2024-04-01 22:04:31 +0900

    :broom: sentry 코드 삭제 추후 추가 예정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import * as Sentry from "@sentry/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { domMax, LazyMotion } from "framer-motion";
@@ -15,28 +14,6 @@ import AuthServiceImpl from "./services/AuthService";
 import GuestServiceImpl from "./services/GuestService";
 import TargetServiceImpl from "./services/TargetService";
 import { routerInfo } from "./utils/router";
-
-Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
-  release: import.meta.env.VITE_RELEASE,
-  integrations: [
-    new Sentry.BrowserTracing({
-      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-      tracePropagationTargets: [import.meta.env.VITE_SERVER_URL],
-    }),
-    new Sentry.Replay(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-
-  beforeSend(event, hint) {
-    console.log(event, hint);
-    return event;
-  },
-});
 
 function App() {
   const queryClient = new QueryClient(QueryClientOptions);
