ad8b8a71ce4ad2c065ce29b29f3c6ad0d2a13a25
CodyMan0 <hys83751952@gmail.com>
2024-04-01 22:50:07 +0900

    :bug: 카카오톡 로그인 endPoint 수정

--- src/api/auth/apis.ts
+++ src/api/auth/apis.ts
@@ -4,13 +4,14 @@ import { AuthResponse, UserInfoType } from "@/types/AuthType";
 const authAPI = {
   /** 카카오 인가 코드 전송 후 로그인 토큰 받아오기 */
   postKakaoCode: async (code: string) => {
-    const { data } = await baseInstance.post<{
-      accessToken: string;
-      refreshToken: string;
-      firstLogin: boolean;
-    }>(`/api/auth/login/kakao?code=${code}`);
+    const { data } = await baseInstance.post<AuthResponse>(`/users/kakao?code=${code}`, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+      },
+    });
     return data;
   },
+  /** 일반 회원 가입 요청 */
   postSignup: async ({ email, password, nickName }: UserInfoType) => {
     const { data } = await baseInstance.post<AuthResponse>("users/signup", {
       email,
@@ -19,6 +20,7 @@ const authAPI = {
     });
     return data;
   },
+  /** 일반 로그인 요청 */
   postSignin: async ({ email, password }: UserInfoType) => {
     const { data } = await baseInstance.post<AuthResponse>("users/signin", {
       email,
