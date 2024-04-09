ac75cc46c2c1c871cc0ff88ff3cb169729e67233
codyman0 <hys83751952@gmail.com>
2024-04-09 23:59:09 +0900

    :sparkles: 기존 불필요한 guest provider 삭제 후 정리

--- src/types/GuestType.ts
+++ src/types/GuestType.ts
@@ -1,20 +1,11 @@
-import { TargetType } from "./TargetTypes";
 
-export interface GuestService {
-	getUserAndTargetNum: () => Promise<UserAndTargetNumType>;
-	getGuestTarget: (id: string | undefined) => Promise<TargetType>;
-	getTargetVote: ({
-		id,
-		success,
-	}: VoteResponseType) => any;
-}
 
 export type VoteResponseType = {
 	id: string | undefined;
 	success: boolean | null | undefined;
 };
 
-export type UserAndTargetNumType = {
+export type UserStatusType = {
 	userCount: string;
 	targetCount: string;
 };
