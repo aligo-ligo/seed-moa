f69469e3777128d385fff0649a1956549d5fcd79
codyman0 <hys83751952@gmail.com>
2024-04-09 23:04:17 +0900

    :speech_balloon: 불필요한 주석 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -21,10 +21,8 @@ import "./styles/global.css";
 
 const queryClient = new QueryClient(QueryClientOptions);
 
-//개발 서버일떄와 서버 일떄 일일이 분기해주는게 맞는걸까?!
 const client = new HttpClient(import.meta.env.VITE_LOCAL_SERVER_URL);
 
-// 의존성 주입 인젝터라는 것이 상황에 따라 다른 의존성 넣으주려고 사용한다.
 const targetService = new TargetServiceImpl(client.withToken());
 const guestService = new GuestServiceImpl(client.withoutToken());
 const routerObject = createBrowserRouter(routerInfo);
