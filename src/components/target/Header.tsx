2554fd1ebeac30d674f47caa71f3f7a7042e1cca
codyman0 <hys83751952@gmail.com>
2024-04-10 00:43:25 +0900

    :recycle: image alt 변경 및 주석 추가

--- src/components/target/Header.tsx
+++ src/components/target/Header.tsx
@@ -7,17 +7,18 @@ type Props = {
 };
 
 const Header = ({ name }: Props) => {
+  // HERE : HEADER 컴포넌트 재설계
   const isNameExisted = !!name === true;
   const { openSideBar } = usePopUp();
 
   return (
-    <div className="flex py-6 justify-between items-center w-full h-full">
+    <div className="flex py-6 justify-between items-center w-full">
       <div className="flex justify-center items-center gap-2">
         <picture>
           <source srcSet={IMAGE_MAP.oliIconWithWebp} type="image/webp" />
           <img
             src={IMAGE_MAP.oliIcon}
-            alt="자그마한 로고 사진"
+            alt="lightweight-logo-image"
             className="phone:w-10 phone:border cursor-pointer desktop:w-12 desktop:border-2  p-1 rounded-xl"
             onClick={openSideBar}
             loading="lazy"
