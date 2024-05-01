b491ae12ed5b49bca775f23839202359ed9efd14
codyman0 <hys83751952@gmail.com>
2024-05-01 22:52:06 +0900

    :sparkles: 목표 생성시 메인 목표 데이터 캐시 무효화 기능 추가

--- src/hooks/api/target/useCreateTarget.tsx
+++ src/hooks/api/target/useCreateTarget.tsx
@@ -1,9 +1,11 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
+import targetOptions from "@/api/target/queryOptions";
 import { authInstance } from "@/libs/api";
 import { CreateTargetResponse } from "../../../types/TargetTypes";
 
 const useCreateTarget = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: ({ goal, subGoal, routine, endDate }: any) =>
       authInstance.post<CreateTargetResponse>("/target/create", {
@@ -12,6 +14,16 @@ const useCreateTarget = () => {
         routine,
         endDate,
       }),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: targetOptions.all,
+        refetchType: "all",
+      });
+    },
+    onError: () => {
+      //TODO: ERROR toast 구현
+      console.log("error");
+    },
   });
 };
 
