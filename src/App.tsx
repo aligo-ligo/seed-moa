b9ae94c5c499de407b71712b4832c1a5d7edefaf
codyman0 <hys83751952@gmail.com>
2024-05-09 23:56:41 +0900

    :bug: 깃헙 시크릿키 재발급후 시도

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,10 @@ import BackgroundMusic from '@/assets/background.mp3';
 import Background from './components/Background';
 import { ToastContainer } from './components/common/toast';
 import Audio from './components/feature/detail/Audio';
-import IMAGE_MAP from './constants/image';
 
 function App() {
   return (
-    <Background imageUrl={IMAGE_MAP.background}>
+    <Background>
       <Outlet />
       <Audio src={BackgroundMusic} />
       <ToastContainer />
