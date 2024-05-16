4cc3ee87f18c15903bdaccc501ca109dc531cc3d
codyman0 <hys83751952@gmail.com>
2024-05-16 18:23:31 +0900

    :sparkles: 401 에러 임시 예외처리

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -33,8 +33,12 @@ authInstance.interceptors.response.use(
   },
   async (error) => {
     const { config } = error;
+    if (error.response.status === 401) {
+      window.location.href = '/';
+    }
+
     if (isAxiosError(error)) {
-      switch (error.response?.data) {
+      switch (error.response?.data.error) {
         case ERROR_RESPONSES.accessExpired: {
           const res = await authAPI.getReissue();
           localStorage.setItem(STORAGE_KEYS.accessToken, res.accessToken);
