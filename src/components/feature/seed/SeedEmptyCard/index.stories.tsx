4d13e4a76fc56e625aaba9507015539a90fdfed6
codyman0 <hys83751952@gmail.com>
2024-05-19 00:57:00 +0900

    :bug: 린트 에러 수정

--- src/components/feature/seed/SeedEmptyCard/index.stories.tsx
+++ src/components/feature/seed/SeedEmptyCard/index.stories.tsx
@@ -15,26 +15,6 @@ export default meta;
 
 type Story = StoryObj<typeof SeedEmptyCard>;
 
-const mockInActiveSeed = {
-  completedRoutineCount: 1,
-  endDate: '2024-05-15',
-  id: 145,
-  routineInfos: [
-    {
-      value: 'teset',
-    },
-    {
-      value: 'teset',
-    },
-    {
-      value: 'teset',
-    },
-  ],
-  seed: 'ㅁㄷㄹㅁㄷ',
-  seedState: 'SEED' as const,
-  startDate: '2024-05-17',
-};
-
 export const Active: Story = {
   render: () => (
     <div className="w-[410px] layout overflow-auto bg-gray-10 scroll-smooth flex flex-col gap-6">
