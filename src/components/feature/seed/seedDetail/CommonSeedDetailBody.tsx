f5d7d1565772d32e32f7185fc46c0d5921c3d457
codyman0 <hys83751952@gmail.com>
2024-05-18 15:36:06 +0900

    :white_check_mark: 응원하기 기능 상세 페이지 임시 추가

--- src/components/feature/seed/seedDetail/CommonSeedDetailBody.tsx
+++ src/components/feature/seed/seedDetail/CommonSeedDetailBody.tsx
@@ -1,6 +1,9 @@
+import SunIcon from '@/assets/icon/SunIcon';
+import Button from '@/components/common/button/Button';
 import { Tag } from '@/components/common/tag';
 import { ToolTip } from '@/components/common/toolTip';
 import { Typography } from '@/components/common/typography/Typography';
+import useToast from '@/hooks/useToast';
 import { DetailSeedType } from '@/types/target/type';
 import { getDateFromDiff } from '@/utils/date';
 import ObserverExitEvent from '../../detail/animatedBox/OpacityBox';
@@ -14,6 +17,7 @@ type CommonSeedDetailBodyType = {
 
 /** 유저와 게스트 공통으로 사용하는 컴포넌트 */
 const CommonSeedDetailBody = ({ seed }: CommonSeedDetailBodyType) => {
+  const toast = useToast();
   const totalRoutineCount =
     getDateFromDiff(seed.endDate, seed.startDate) * seed.routineDetails.length;
 
@@ -33,8 +37,23 @@ const CommonSeedDetailBody = ({ seed }: CommonSeedDetailBodyType) => {
               </ObserverExitEvent>
             </div>
           </div>
-
           <Sticker>{detailSeedStateObj[seed.seedState]}</Sticker>
+
+          <div className="flex w-full justify-end">
+            <Sticker>
+              <Button
+                className="bg-gray-10"
+                onClick={() => {
+                  toast({
+                    type: 'default',
+                    message: '응원해준 친구을 볼 수 있는 기능 준비중입니다!',
+                  });
+                }}
+              >
+                <SunIcon width={30} height={30} />
+              </Button>
+            </Sticker>
+          </div>
         </div>
         {/* //TODO : 좋아요 UI 표현 고민해보자 */}
         <TaskList tasks={seed.routineDetails} />
