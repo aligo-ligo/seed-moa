c9cc06386c89f9311b20fc06f8cb0092c344e61e
codyman0 <hys83751952@gmail.com>
2024-05-19 02:17:32 +0900

    :sparkles: 센트리 최상위 컴포넌트 내부, 초기화 로직 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ReactDOM from 'react-dom/client';
@@ -7,6 +8,24 @@ import queryClient from './constants/queryClient';
 import ToastProvider from './context/toast/ToastProvider';
 import './styles/global.css';
 import { routerInfo } from './utils/router';
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+      networkDetailAllowUrls: [import.meta.env.VITE_SERVER_URL],
+    }),
+  ],
+  environment: import.meta.env.PROD ? 'production' : 'development',
+  enabled: import.meta.env.PROD,
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ['localhost'],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 const routerObject = createBrowserRouter(routerInfo);
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import react from "@vitejs/plugin-react";
-import { visualizer } from "rollup-plugin-visualizer";
-import { defineConfig, type PluginOption } from "vite";
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig, type PluginOption } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), visualizer() as PluginOption],
-    resolve: {
-        alias: [
-          { find: "@", replacement: "/src" },
-        ],
-      },
+  plugins: [
+    react(),
+    visualizer() as PluginOption
+  ],
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+  },
+
+
 });
