2058d9c088b21aa4471b1247ea42ca56c8d01981
codyman0 <hys83751952@gmail.com>
2024-05-20 23:27:35 +0900

    :white_check_mark: 로그인 후, isFirst 프로퍼티를 활용한 유저 리다이렉트 로직 임시 구현

--- src/hooks/auth/useAuth.ts
+++ src/hooks/auth/useAuth.ts
@@ -1,17 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
-import { useCallback } from "react";
-import { useNavigate } from "react-router-dom";
+import { useMutation } from '@tanstack/react-query';
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import authAPI from "@/api/auth/apis";
-import { ROUTER_PATHS } from "@/constants/routerPath";
-import STORAGE_KEYS from "@/constants/storageKeys";
-import useAuthStore from "@/store/authStore";
-import useToast from "../useToast";
+import authAPI from '@/api/auth/apis';
+import { ROUTER_PATHS } from '@/constants/routerPath';
+import STORAGE_KEYS from '@/constants/storageKeys';
+import useAuthStore from '@/store/authStore';
+import useToast from '../useToast';
 
 const useAuth = () => {
-  const toast = useToast()
-  const navigate = useNavigate()
-  const {isLoggedIn, setIsLoggedIn} = useAuthStore()
+  const toast = useToast();
+  const navigate = useNavigate();
+  const { isLoggedIn, setIsLoggedIn } = useAuthStore();
   const { mutateAsync } = useMutation({ mutationFn: authAPI.postKakaoCode });
 
   const kakaoLogin = useCallback(
@@ -24,18 +24,19 @@ const useAuth = () => {
         const data = await mutateAsync(authorizeCode);
         localStorage.setItem(STORAGE_KEYS.accessToken, data.tokenInfo.accessToken as string);
         localStorage.setItem(STORAGE_KEYS.refreshToken, data.tokenInfo.refreshToken as string);
-        setIsLoggedIn(true)
-        setTimeout(() => 
-        {
-          navigate(ROUTER_PATHS.TARGET)
-          toast({message:'LOGIN_SUCCESS'})
-        }, 1000);
-        
+        setIsLoggedIn(true);
+        console.log('ONBOARDING',data.isFirst)
+        // 테스트를 위한 임시 변수
+        const isFirst = true
+        if (isFirst) {
+          navigate(ROUTER_PATHS.ONBOARDING)
+          return
+        }
+        navigate(ROUTER_PATHS.TARGET);
+        toast({ message: 'LOGIN_SUCCESS' });
       } catch {
-        setTimeout(() => {
-          navigate(ROUTER_PATHS.ROOT)
-          toast({message:'LOGIN_FAIL'})
-        }, 1000); 
+        navigate(ROUTER_PATHS.ROOT);
+        toast({ message: 'LOGIN_FAIL' });
       }
     },
     [mutateAsync, navigate, setIsLoggedIn, toast],
@@ -44,12 +45,12 @@ const useAuth = () => {
   const handleLogout = () => {
     localStorage.removeItem(STORAGE_KEYS.accessToken);
     localStorage.removeItem(STORAGE_KEYS.refreshToken);
-    setIsLoggedIn(false)
+    setIsLoggedIn(false);
     navigate(ROUTER_PATHS.ROOT);
-    toast({message:'LOGOUT_SUCCESS'})
+    toast({ message: 'LOGOUT_SUCCESS' });
   };
-    
-  return {isLoggedIn, login:kakaoLogin , logout : handleLogout}
-}
 
-export default useAuth
+  return { isLoggedIn, login: kakaoLogin, logout: handleLogout };
+};
+
+export default useAuth;
