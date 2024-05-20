2dee6aa5a1859c1c7d2d244d7057d10a502eb697
codyman0 <hys83751952@gmail.com>
2024-05-20 22:56:41 +0900

    :recycle: 불필요한 쉐어 컨텍스트 삭제 후, useQueryString 훅을 활용하여 공유 판별 로직 단순화

--- src/components/feature/detail/Task.tsx
+++ src/components/feature/detail/Task.tsx
@@ -9,9 +9,9 @@ import Logo from '@/assets/logo/Logo';
 import { Typography } from '@/components/common/typography/Typography';
 import { DELAY_SECOND } from '@/constants/contants';
 import { useRoutineContext } from '@/context/RoutineContext';
-import { useSharedStateContext } from '@/context/SharedStateContext';
 import useRoutineTitleMutation from '@/hooks/seed/routine/useRoutineTitleMutation';
 import { useInput } from '@/hooks/useInput';
+import useQueryString from '@/hooks/useQueryString';
 import useMusicStore from '@/store/useMusicStore';
 import { TaskEditInput } from './TaskEditInput';
 interface TaskProps {
@@ -26,7 +26,7 @@ const Task = ({ routineTitle, routineId, completedRoutineToday, onFinishRoutine 
   const toggleMusicPlaying = useMusicStore((s) => s.togglePlaying);
   const isPlaying = useMusicStore((s) => s.isPlaying);
   const { onRainBgOpen, onRainBgClose } = useRoutineContext();
-  const { isShared } = useSharedStateContext();
+  const { isShared } = useQueryString('share');
 
   const { id } = useParams();
   const [isEditing, setIsEditing] = useState(false);
@@ -50,7 +50,6 @@ const Task = ({ routineTitle, routineId, completedRoutineToday, onFinishRoutine 
 
   return (
     <>
-      {/* TODO : 합성 컴포넌트로 만들면 좋을 듯, 지금은 우선 Props로 최상위는 공유 됐는지 안됐는지 그리고 그 안에서 수정이 눌렸는지를 구분하고 있음 */}
       {isShared ? (
         <div className="w-full flex gap-1 items-start px-4 py-3 rounded-[8px] border-gray-20 bg-white shadow-thumb">
           <Typography type="body2">{routineTitle}</Typography>
