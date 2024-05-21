cdb85ebf128a6f084ef3f14b5d0340baac818c9b
codyman0 <hys83751952@gmail.com>
2024-05-21 11:08:01 +0900

    :lipstick: 부자연스러운 비 배경 제거

--- src/components/feature/detail/RainBackGround.tsx
+++ src/components/feature/detail/RainBackGround.tsx
@@ -5,10 +5,8 @@ const RainBackGround = ({ isRainOpen }: { isRainOpen: boolean }) => {
     <>
       {isRainOpen && (
         <>
-          <Rain size={10} position={{ x: 'left-[10px]', y: 'top-0' }} />
+          <Rain size={10} position={{ x: 'left-[10px]', y: 'top-[-10px]' }} />
           <Rain size={22} position={{ x: 'right-[80px]', y: 'top-[10px]' }} />
-          <Rain size={22} position={{ x: 'right-[200px]', y: 'top-[-100px]' }} />
-          <Rain size={22} position={{ x: 'left-[100px]', y: 'top-[-80px]' }} />
           <Rain size={10} position={{ x: 'left-[20px]', y: 'top-[20px]' }} />
           <Rain size={22} position={{ x: 'left-[80px]', y: 'top-[80px]' }} />
           <Rain size={4} position={{ x: 'left-[140px]', y: 'top-[50px]' }} />
@@ -18,6 +16,7 @@ const RainBackGround = ({ isRainOpen }: { isRainOpen: boolean }) => {
           <Rain size={10} position={{ x: 'right-[150px]', y: 'top-[100px]' }} />
           <Rain size={22} position={{ x: 'left-[30px]', y: 'top-[200px]' }} />
           <Rain size={8} position={{ x: 'left-[200px]', y: 'top-[250px]' }} />
+          <Rain size={16} position={{ x: 'right-[10px]', y: 'top-[100px]' }} />
         </>
       )}
     </>
