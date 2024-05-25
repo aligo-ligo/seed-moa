3f6269199e445cdc852a3d0cafdc35d52a238afa
codyman0 <hys83751952@gmail.com>
2024-05-25 23:29:24 +0900

    :zap: 달력 input 및 버튼 추가 속성

--- src/components/feature/create/DatePicker/DatePickerComponent.tsx
+++ src/components/feature/create/DatePicker/DatePickerComponent.tsx
@@ -8,29 +8,35 @@ const DatePickerComponent = () => {
   const { register, setValue, getValues } = useFormContext();
   const endDateValue = getValues('endDate');
   return (
-    <DatePicker
-      dateFormat={`yyyy년 MM월 dd일`}
-      className="flex-1 placeholder:text-gray-100 h-10 outline-none border-b w-full border-gray-200 bg-transparent"
-      {...register('endDate')}
-      selected={endDateValue}
-      onChange={(date) => setValue('endDate', date, { shouldValidate: true })}
-      placeholderText="클릭해서 날짜를 선택해보세요"
-      isClearable
-      withPortal
-      minDate={new Date(fromNowOfMinDays)}
-      maxDate={new Date(fromNowOfMaxDays)}
-      autoComplete="off"
-      renderCustomHeader={({ date, decreaseMonth, increaseMonth }) => (
-        <CalenderHeader date={date} decreaseMonth={decreaseMonth} increaseMonth={increaseMonth} />
-      )}
-      dayClassName={(date) =>
-        getKrDateName(date) === '토요일'
-          ? 'saturday'
-          : getKrDateName(date) === '일요일'
-            ? 'sunday'
-            : null
-      }
-    />
+    <>
+      <label htmlFor="달력" className="sr-only">
+        달력
+      </label>
+      <DatePicker
+        id="달력"
+        dateFormat={`yyyy년 MM월 dd일`}
+        className="flex-1 placeholder:text-gray-100 h-10 outline-none border-b w-full border-gray-200 bg-transparent"
+        {...register('endDate')}
+        selected={endDateValue}
+        onChange={(date) => setValue('endDate', date, { shouldValidate: true })}
+        placeholderText="클릭해서 날짜를 선택해보세요"
+        isClearable
+        withPortal
+        minDate={new Date(fromNowOfMinDays)}
+        maxDate={new Date(fromNowOfMaxDays)}
+        autoComplete="off"
+        renderCustomHeader={({ date, decreaseMonth, increaseMonth }) => (
+          <CalenderHeader date={date} decreaseMonth={decreaseMonth} increaseMonth={increaseMonth} />
+        )}
+        dayClassName={(date) =>
+          getKrDateName(date) === '토요일'
+            ? 'saturday'
+            : getKrDateName(date) === '일요일'
+              ? 'sunday'
+              : null
+        }
+      />
+    </>
   );
 };
 
